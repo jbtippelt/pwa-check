00647441c610afc95809f2d69b70caa8a6532f86
jbtippelt <git@jakobtippelt.de>
2020-05-11 15:24:53 +0200

    Switch to real material cdk usage

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PWA Check';
+
+  ngOnInit() {
+    if(environment.production) {
+      alert("Under development!")
+    }
+  }
 }
